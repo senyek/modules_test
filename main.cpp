d605c1d4546d0bdf0d666b16f454ea5f3655cc2f
Jon Shute <jonshute@gmail.com>
2023-08-31 15:23:11 +0100

    initial version for some C++ module testing

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+import test_module;
+
+
+int main()
+{
+    fishcake f;
+    f.do_something();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
---
+++ test_module.ixx
@@ -0,0 +1,14 @@
+export module test_module;
+
+import <string>;
+
+export class fishcake
+{
+public:
+    void do_something();
+};
+
+void fishcake::do_something()
+{
+    
+}
